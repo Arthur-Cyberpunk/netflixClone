03ceb751838a49f3fa35293ef686a771fc27e7ad
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2022-07-27 15:00:46 -0300

    added call api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import Tmdb from "./Tmdb";
+
 function App() {
+  const [movieList, setMovieList] = useState([]);
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    };
+
+    loadAll();
+  }, []);
   return <div></div>;
 }
 
