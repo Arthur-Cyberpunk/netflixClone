847bc028dcd8aed7c4a3ce82d5c7c8e33ac0e052
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2022-07-27 17:39:59 -0300

    added series information list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import FeatureMovie from "./components/FeatureMovie";
 import MovieRow from "./components/MovieRow";
 import Tmdb from "./Tmdb";
 
 function App() {
   const [movieList, setMovieList] = useState([]);
+  const [featureData, setFeatureData] = useState(null);
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1),
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
     };
 
     loadAll();
   }, []);
   return (
     <div className="page">
+      {featureData && <FeatureMovie item={featureData} />}
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
---
+++ src/components/FeatureMovie.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FeatureMovie.css";
+
+const FeatureMovie = () => {
+  return <div></div>;
+};
+
+export default FeatureMovie;
