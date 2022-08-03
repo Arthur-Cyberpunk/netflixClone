acf93977e3f7e0d7dd96a1f888ea205b3f222a06
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2022-08-03 11:21:59 -0300

    added loading

--- src/App.js
+++ src/App.js
@@ -58,6 +58,15 @@ function App() {
         <br />
         Dados pegos do site Themoviedb.org
       </footer>
+
+      {movieList.length <= 0 && (
+        <div className="loading">
+          <img
+            src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif"
+            alt="Carregando"
+          ></img>
+        </div>
+      )}
     </div>
   );
 }
